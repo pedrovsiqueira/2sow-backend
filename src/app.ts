50866ec7d964ac7820d8c250d7b940717f2fe983
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-15 20:49:39 -0300

    feat: update app to use express, cors and custom routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-require('dotenv').config();
-
+import './database/connection'
+import userRoutes from './routes/users.routes'
+import adminRoutes from './routes/admin.routes'
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
+app.use(express.json())
+
+app.use(cors())
+app.use('/api/usuarios', userRoutes);
+app.use('/api/auth', adminRoutes);
 
 export default app;
