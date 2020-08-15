73d24935150ca8bd0c78c8aae5e91bd0a211a088
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-15 20:49:56 -0300

    fix: update config to call env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app  from './app';
+import { PORT } from './configs/env';
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(` 2sow is listening on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`2sow is listening on port ${PORT}`);
 });
